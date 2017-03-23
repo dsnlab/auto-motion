4199cc384ade13abad56139d3632778730f04e5f
Dani Cosme <dani.cosme@gmail.com>
2017-03-23 14:46:19 -0700

    updated trash tresholds

--- auto_trash.R
+++ auto_trash.R
@@ -46,45 +46,84 @@ study = "FP"
 rpPattern = "^rp_(FP[0-9]{3})_(.*).txt"
 rpCols = c("euclidian_trans","euclidian_rot","euclidian_trans_deriv","euclidian_rot_deriv","trash")
 
-# write new rp_txt files?
+# write new rp_txt and plots files?
 writeRP = TRUE
 writePlots = TRUE
 
 #------------------------------------------------------
-# load data
+# load global intensity data
 #------------------------------------------------------
 # global intensity file created using calculate_global_intensities.R
 intensities = read.csv(paste0(outputDir,study,'_globalIntensities.csv'))
 
 #------------------------------------------------------
-# create trash regressors
+# load rp_txt files and concatenate them
 #------------------------------------------------------
-trash = intensities %>% group_by(subjectID, run) %>%
+# generate file list
+file_list = list.files(rpDir, pattern = rpPattern)
+
+for (file in file_list){
+  # if the merged dataset doesn't exist, create it
+  if (!exists("dataset")){
+    temp = read.table(paste0(rpDir,file))
+    colnames(temp) = c("euclidian_trans","euclidian_rot","euclidian_trans_deriv","euclidian_rot_deriv","trash")
+    dataset = data.frame(temp, file = rep(file,count(temp))) %>% 
+      mutate(volume = row_number()) %>%
+      extract(file,c("subjectID","run"), rpPattern)
+  }
+  
+  # if the merged dataset does exist, append to it
+  else {
+    temp_dataset = read.table(paste0(rpDir,file))
+    colnames(temp_dataset) = c("euclidian_trans","euclidian_rot","euclidian_trans_deriv","euclidian_rot_deriv","trash")
+    temp_dataset = data.frame(temp_dataset, file = rep(file,count(temp_dataset))) %>% 
+      mutate(volume = row_number()) %>%
+      extract(file,c("subjectID","run"), rpPattern)
+    dataset = rbind(dataset, temp_dataset)
+    rm(temp_dataset)
+  }
+}
+
+#------------------------------------------------------
+# merge and create trash regressors
+#------------------------------------------------------
+trash = intensities %>% 
+  mutate(subjectID = as.character(subjectID),
+         run = as.character(run)) %>%
+  left_join(., dataset, by = c("subjectID", "run", "volume")) %>%
+  group_by(subjectID, run) %>%
   mutate(Diff.mean = volMean - lag(volMean),
-         Diff.sd = volSD - lag(volSD)) %>%
+         Diff.sd = volSD - lag(volSD),
+         Diff.intensity = volMean - mean(volMean)) %>%
   ungroup %>%
   mutate(meanDiff.mean = mean(Diff.mean, na.rm=TRUE),
          sdDiff.mean = sd(Diff.mean, na.rm=TRUE),
-         thresholdDiff.mean = sdDiff.mean*2, 
          meanDiff.sd = mean(Diff.sd, na.rm=TRUE),
          sdDiff.sd = sd(Diff.sd, na.rm=TRUE),
-         thresholdDiff.sd = sdDiff.sd*3, 
+         # meanDiff.intensity = mean(Diff.intensity, na.rm=TRUE),
+         # sdDiff.intensity = sd(Diff.intensity, na.rm=TRUE),
+
+         # code volumes above mean thresholds as trash
+         trash.auto = ifelse(Diff.mean > (meanDiff.mean + 3*sdDiff.mean) | Diff.mean < (meanDiff.mean - 1.5*sdDiff.mean), 1, 0),
+         trash.auto = ifelse(Diff.sd > (meanDiff.sd + 3*sdDiff.sd) | Diff.sd < (meanDiff.sd - 3*sdDiff.sd), 1, trash.auto),
+         # trash.auto = ifelse(Diff.intensity > (meanDiff.intensity + 2*sdDiff.intensity) | Diff.intensity < (meanDiff.intensity - 2*sdDiff.intensity), 1, trash.auto),
          
-         # code volumes above mean and SD thresholds as trash
-         trashDiff = ifelse(Diff.mean > (meanDiff.mean + thresholdDiff.mean) | Diff.mean < (meanDiff.mean - thresholdDiff.mean), 1, 0),
-         trashDiff = ifelse(Diff.sd > (meanDiff.sd + thresholdDiff.sd) | Diff.sd < (meanDiff.sd - thresholdDiff.sd), 1, trashDiff),
+         # code volumes with more than +/- .3mm translation in Euclidian distance
+         trash.auto = ifelse(euclidian_trans_deriv > .3 | euclidian_trans_deriv < -.3, 1, trash.auto),
+
+         # code volumes with more than +/- .3mm translation in Euclidian distance
+         trash.auto = ifelse(euclidian_rot_deriv > .3 | euclidian_rot_deriv < -.3, 1, trash.auto),
          
          # recode as trash if volume behind and in front are both marked as trash
-         trashDiff = ifelse(trashDiff == 0 & lag(trashDiff) == 1 & lead(trashDiff) == 1, 1, trashDiff),
-         behind = lead(trashDiff), 
-         ahead = lag(trashDiff), 
-         
-         # reduce false negatives before trash volume
-         trashDiff = ifelse((trashDiff == 0 & lead(trashDiff == 1)) & (Diff.mean > (meanDiff.mean + .5*thresholdDiff.mean) | Diff.mean < (meanDiff.mean - .5*thresholdDiff.mean)), 1, trashDiff),
+         trash.auto = ifelse(trash.auto == 0 & lag(trash.auto) == 1 & lead(trash.auto) == 1, 1, trash.auto),
          
          # reduce false positives on last volume in motion sequence
-         trashDiff = ifelse((trashDiff == 1 & lead(trashDiff == 0)) & (Diff.mean < (meanDiff.mean + 1.5*thresholdDiff.mean) & Diff.mean > (meanDiff.mean - 1.5*thresholdDiff.mean)), 0, trashDiff)) %>%
-  select(subjectID, run, volume, Diff.mean, Diff.sd, volMean, volSD, trashDiff)
+         trash.auto = ifelse((trash.auto == 1 & lag(trash.auto == 1) & lead(trash.auto == 0)) & (Diff.mean < (meanDiff.mean + 1.5*sdDiff.mean) & Diff.mean > (meanDiff.mean - 3*sdDiff.mean)), 0, trash.auto),
+         #trash.auto = ifelse((trash.auto == 1 & lead(trash.auto == 0)) & (euclidian_trans_deriv < .1 & euclidian_trans_deriv > -.1), 0, trash.auto),
+
+         # reduce false negatives before trash volume
+         trash.auto = ifelse((trash.auto == 0 & lead(trash.auto == 1)) & (Diff.mean > (meanDiff.mean + sdDiff.mean) | Diff.mean < (meanDiff.mean - sdDiff.mean)), 1, trash.auto)) %>%
+  select(subjectID, run, volume, Diff.mean, Diff.sd, volMean, volSD, starts_with("euclidian"), trash.auto)
 
 #------------------------------------------------------
 # write csv
@@ -92,72 +131,30 @@ trash = intensities %>% group_by(subjectID, run) %>%
 write.csv(trash, paste0(outputDir,study,'_autoTrash.csv'), row.names = FALSE)
 
 #------------------------------------------------------
-# merge with rp_txt and write new trash regressors
+# write rp_txt files
 #------------------------------------------------------
 if (writeRP){
-  #------------------------------------------------------
-  # load rp_txt files and concatenate them
-  #------------------------------------------------------
-  # generate file list
-  file_list = list.files(rpDir, pattern = rpPattern)
+rp = trash %>%
+  select(subjectID, run, volume, starts_with("euclidian"), trash.auto) %>%
+  mutate(trash.auto = ifelse(is.na(trash.auto), 0, trash.auto))
   
-  for (file in file_list){
-    # if the merged dataset doesn't exist, create it
-    if (!exists("dataset")){
-      temp = read.table(paste0(rpDir,file))
-      colnames(temp) = c("euclidian_trans","euclidian_rot","euclidian_trans_deriv","euclidian_rot_deriv","trash")
-      dataset = data.frame(temp, file = rep(file,count(temp))) %>% 
-        mutate(volume = sprintf("%04d",row_number())) %>%
-        extract(file,c("subjectID","run"), rpPattern)
-    }
-    
-    # if the merged dataset does exist, append to it
-    else {
-      temp_dataset = read.table(paste0(rpDir,file))
-      colnames(temp_dataset) = c("euclidian_trans","euclidian_rot","euclidian_trans_deriv","euclidian_rot_deriv","trash")
-      temp_dataset = data.frame(temp_dataset, file = rep(file,count(temp_dataset))) %>% 
-        mutate(volume = sprintf("%04d",row_number())) %>%
-        extract(file,c("subjectID","run"), rpPattern)
-      dataset = rbind(dataset, temp_dataset)
-      rm(temp_dataset)
-    }
-  }
-  
-  #------------------------------------------------------
-  # merge trash regressors
-  #------------------------------------------------------
-  trash.merge = trash %>% 
-    select(subjectID, run, volume, trashDiff) %>%
-    mutate(subjectID = as.character(subjectID),
-           run = as.character(run))
-  
-  rp = dataset %>%
-    mutate(volume = as.integer(volume)) %>%
-    left_join(., trash.merge, by = c("subjectID", "run", "volume")) %>%
-    mutate(trashCombined = ifelse(is.na(trashDiff), 0, 
-                                  ifelse(trash == 1 | trashDiff == 1, 1, 0))) %>%
-    select(subjectID, run, volume, starts_with("euclidian"), trashCombined)
-  
-  #------------------------------------------------------
-  # write rp_txt files
-  #------------------------------------------------------
-  rp_files_written = rp %>% 
-    arrange(subjectID, run, volume) %>% 
-    group_by(subjectID, run) %>% 
-    do({
-      fname=paste(
-        rpOutputDir,
-        'rp_',.$subjectID[[1]],'_',.$run[[1]],'.txt',
-        sep='')
-      write.table(
-        .[,c(-1,-2,-3)],
-        fname,
-        quote=F,
-        sep='   ',
-        row.names=F,
-        col.names=F)
-      data.frame(rp_file_name=fname)
-    })
+rp_files_written = rp %>% 
+  arrange(subjectID, run, volume) %>% 
+  group_by(subjectID, run) %>% 
+  do({
+    fname=paste(
+      rpOutputDir,
+      'rp_',.$subjectID[[1]],'_',.$run[[1]],'.txt',
+      sep='')
+    write.table(
+      .[,c(-1,-2,-3)],
+      fname,
+      quote=F,
+      sep='   ',
+      row.names=F,
+      col.names=F)
+    data.frame(rp_file_name=fname)
+  })
 }
 
 #------------------------------------------------------
@@ -168,8 +165,8 @@ if (writePlots){
   trash.plot = dataset %>%
     mutate(volume = as.integer(volume)) %>%
     left_join(., trash, by = c("subjectID", "run", "volume")) %>%
-    mutate(code = ifelse(trashDiff == 1 & trash == 1, 2, 
-                         ifelse(trashDiff == 1 & trash == 0, 3, trash))) %>%
+    mutate(code = ifelse(trash.auto == 1 & trash == 1, 2, 
+                         ifelse(trash.auto == 1 & trash == 0, 3, trash))) %>%
     select(-starts_with("euclidian"), -starts_with("Diff"), -starts_with("trash")) %>%
     gather(measure, value, -c(subjectID, run, volume, code))
   
