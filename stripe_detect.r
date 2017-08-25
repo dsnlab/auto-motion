4bc82933b0b6c42f506731560983f88252854f7f
John Flournoy <uoflournoy@gmail.com>
2017-08-25 14:49:26 -0700

    ready for testing on the grid

--- stripe_detect.r
+++ stripe_detect.r
@@ -1,4 +1,80 @@
-#Functions for doing the work -- these would be packegized
+#!/usr/bin/env Rscript
+# This script loads functional volumes, generates info for detecting stripes,
+# and returns a csv file specified in the options below.
+# 
+# Inputs:
+# * subjectDir = path to subject directory
+# * functionalDir = path from subject's directory to to functional files
+# * outputDir = path where study_globalIntensities.csv will be written
+# * study = study name
+# * subPattern = regular expression for subject IDs
+# * prefix = SPM prefix appended to functional images; use "" to ignore
+# * runPattern = regular expression for run names; use "" to specify all directories in $functionalDir
+# * final_output_csv = path and file name for 'study_globalIntensities.csv'
+# * parallelize = use TRUE to parallelize locally only, FALSE if not
+# * leave_n_free_cores = number of cores to leave free
+#
+# Outputs:
+# * study_globalIntensities.csv = CSV file with global intensity value for each image
+
+#Command-line form:
+#  Rscript script.R [index]
+#or
+#  Rscript script.R filecount
+#where "index" is a number indexing the row for this particular call.
+#If the command line argument exists, we turn off multi-core, assuming that
+#the script is being run once per file across multiple CPUs on one or more
+#HPC nodes.
+#
+#The option "filecount" allows the script to merely return the numbre of
+#files to be processed so one can set the HPC script up properly
+options(warn=-1)
+args = commandArgs(trailingOnly=T)
+message(args)
+# test if there is at least one argument: if not, return an error
+if (length(args)==0) {
+  file_n_only=F
+  index=NA #not using nodes
+} else if (length(args)==1 & !is.na(as.numeric(args[1]))) {
+  #args is a single numeric value
+  file_n_only=F
+  index=args[1]
+} else if (length(args)==1 & args[1] == 'filecount') {
+  file_n_only=T
+  index=NA
+} else {
+  stop("Wrong arguments supplied.
+Command-line form:
+  Rscript script.R [index]
+or
+  Rscript script.R filecount")
+}
+options(warn=0)
+#------------------------------------------------------
+# define variables
+# these variables are all you should need to change
+# to run the script
+#------------------------------------------------------
+
+# paths
+subjectDir = "~/code_new/automotion-test-set/vids/projects/dsnlab/tds/fMRI/subjects_tds2/"
+functionalDir = "" #e.g., "/ppc/functionals/"
+outputDir = "~/code_new/automotion-test-set/output/" 
+
+# variables
+study = "tds"
+nii4dFilePattern = "_ru.*_4d.nii.gz"
+subPattern = "[0-9]{3}"
+prefix = "_ru" #"o" 
+runPattern = "(?:cyb|stop|vid)[1-8]" #"^run*" 
+final_output_csv = file.path(outputDir,paste0(study,'_stripes.csv'))
+parallelize = is.na(index)
+leave_n_free_cores = 1
+
+#------------------------------------------------------
+# Functions
+#------------------------------------------------------
+
 quickscale <- function(timeseries){
   vmin <- min(timeseries)
   vmax <- max(timeseries)
@@ -44,18 +120,16 @@ normalize_nii <- function(aNii) {
   scaled_img[abox[[1]],abox[[2]],abox[[3]],]
 }
 
-getPSD <- function(aNiceNii, margins = c(1,4), parallel = T, mc.cores = 8){
-  library(parallel)
+getPSD <- function(aNiceNii, margins = c(1,4), parallel = T, aCluster = NA){
+  #parlallel = T helps this go fast on a local computer
   if(parallel){
-    cl <- makeCluster(mc.cores)
-    somepower <- parApply(cl, aNiceNii, margins, welchPSD_from_slice)
-    stopCluster(cl)
+    if(length(aCluster) == 1 && is.na(aCluster)) aCluster <- makeCluster(getOption("mc.cores", 2L))
+    somepower <- parApply(aCluster, aNiceNii, margins, welchPSD_from_slice)
   } else {
     somepower <- apply(aNiceNii, margins, welchPSD_from_slice)
   }
-  slice_power <- apply(somepower, 2, bind_rows)
-  
-  slice_power_per_t <- bind_rows(lapply(as.list(1:length(slice_power)),
+  slice_power <- apply(somepower, 2, dplyr::bind_rows)
+  slice_power_per_t <- dplyr::bind_rows(lapply(as.list(1:length(slice_power)),
                                         function(t){
                                           adf <- as.data.frame(slice_power[t])
                                           adf$t <- t
@@ -63,46 +137,107 @@ getPSD <- function(aNiceNii, margins = c(1,4), parallel = T, mc.cores = 8){
                                         }))
 }
 
-#TEMPORARY
-file="~/code_new/automotion-test-set/vids/projects/dsnlab/tds/fMRI/subjects_tds2/102/stop7/_rustop7_4d.nii.gz"
-img = RNifti::readNifti(file, internal = FALSE)
-#temporary
+stripes_for_nii <- function(filepath, parallel = F, aCluster = NA){
+  img = RNifti::readNifti(filepath, internal = FALSE)
+  
+  niceNii <- normalize_nii(img)
+  image(niceNii[floor(dim(niceNii)[1]/2 + 2),,,31])
+  
+  slice_power_per_t <- getPSD(niceNii, parallel = parallel, aCluster = aCluster)
+  
+  #list of frequencies isn't the same every time, so we want to
+  #bin them
+  freq_tiles <- quantile(unique(slice_power_per_t$freq), seq(0, 1, .125))
+  slice_power_per_t$tile <- findInterval(slice_power_per_t$freq, seq(0,.55,.05))
+  slice_power_per_t_reduced <- dplyr::summarize(dplyr::group_by(slice_power_per_t, t, tile), freqtile_power = max(power))
+  return(slice_power_per_t_reduced)
+}
 
-#do the work
-library(tidyverse)
+#------------------------------------------------------
+# Run the things
+#------------------------------------------------------
 
-niceNii <- normalize_nii(img)
-image(niceNii[floor(dim(boxed_scaled_img)[1]/2 + 2),,,31])
+if(parallelize){
+  library(parallel)
+  mc.cores <- parallel::detectCores() - leave_n_free_cores
+  cl <- makeCluster(mc.cores)
+} else {
+  cl <- NA
+}
 
-slice_power_per_t <- getPSD(niceNii)
+extractSubRunPattern <- paste0('.*/*(', subPattern, ')/.*(', runPattern,').*', nii4dFilePattern)
+filelist = data.frame(file = list.files(subjectDir, pattern = nii4dFilePattern, recursive = T))
+fileListDF <- tidyr::extract(data = filelist,
+                             col = file, into = c('subject', 'run'),
+                             regex = paste0('.*/*(', subPattern, ')/.*(', runPattern,').*', nii4dFilePattern),
+                             remove = F)
+fileListDF$subjectDir <- subjectDir
 
-#if you inspect the data frame, it doesn't return the same list of frequencies every time. so I want to
-#group them into octiles to compute average power within each
-freq_tiles <- quantile(unique(slice_power_per_t$freq), seq(0, 1, .125))
-slice_power_per_t$tile <- findInterval(slice_power_per_t$freq, seq(0,.55,.05))
-unique(slice_power_per_t$tile)
 
-#summarize and plot
-#let's ignore the lower frequencies because they don't show motion
-slice_power_per_t %>% 
-  group_by(t, tile) %>%
-  summarize(freqtile_power = max(power)) %>%
-  group_by(tile) %>%
-  mutate(freqtile_power_c = freqtile_power - min(freqtile_power)) %>%
-  filter(tile > 8) %>%
-  mutate(red_zone = freqtile_power_c > .0003,
-         label = ifelse(red_zone, as.character(t), '')) %>%
-  ggplot(aes(x = t, y = freqtile_power_c)) +
-  geom_line(aes(group = tile, alpha = tile), size = .25) +
-  geom_point(aes(group = tile, alpha = tile, color = red_zone, size = red_zone)) +
-  geom_text(aes(label = label), size = 3, position = position_nudge(x = 2, y = .000075)) + 
-  # geom_segment(aes(xend = t, group = tile, alpha = tile), yend = 0) +
-  # coord_trans(y = 'log') +
-  scale_x_continuous(breaks = c(1, seq(5, max(slice_power_per_t$t), 5)), minor_breaks = 1:max(slice_power_per_t$t)) + 
-  scale_color_manual(breaks = c(F, T), values = c('black', 'red')) +
-  scale_alpha_continuous(range = c(.5, 1), breaks = 1:max(slice_power_per_t$tile)) + 
-  scale_size_manual(breaks = c(F, T), values = c(.25, 1)) + 
-  theme(axis.text.x = element_text(size = 6))
-# +
-#   geom_rect(aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), 
-#             data = data.frame(xmin=c(10,80),xmax=c(40, 120), ymin=c(0,0), ymax=c(.016,.016), freqtile_power=c(0,0), t=c(0,0)))
+if(file_n_only){
+  message(paste0("Number of files to process: ", dim(fileListDF)[1]))
+} else {
+  if(!file.exists(outputDir)){
+    dir.create(outputDir)
+  }
+  if(is.na(index)){
+    library(dplyr,tidyr)
+    slice_power_per_t <- fileListDF %>%
+      slice(1:2) %>% ###TESTING
+      group_by(file, subject, run) %>%
+      do({
+        file = paste0(.$subjectDir[[1]], .$file[[1]])
+        message(file)
+        file_power_per_t <- stripes_for_nii(file, parallel = parallelize, aCluster = cl)
+      })
+    write.csv(slice_power_per_t, final_output_csv, row.names = F)
+  } else {
+    #index is set, so only do it for on the of the files.
+    slice_power_per_t <- fileListDF %>%
+      slice(index) %>%
+      group_by(file, subject, run) %>%
+      do({
+        file = paste0(.$subjectDir[[1]], .$file[[1]])
+        message(file)
+        file_power_per_t <- stripes_for_nii(file, parallel = parallelize, aCluster = cl)
+      })
+    if(file.exists(final_output_csv)){
+      write.table(slice_power_per_t, final_output_csv, row.names = F, append = T, sep = ',', col.names = F)  
+    } else {
+      write.csv(slice_power_per_t, final_output_csv, row.names = F)  
+    }
+  }
+  
+  if(FALSE){
+    library(tidyverse)
+    #summarize and plot
+    #let's ignore the lower frequencies because they don't show motion
+    slice_power_per_t  %>%
+      group_by(tile) %>%
+      mutate(freqtile_power_c = freqtile_power - min(freqtile_power)) %>%
+      filter(tile > 8) %>%
+      mutate(red_zone = freqtile_power_c > .0003,
+             label = ifelse(red_zone, as.character(t), '')) %>%
+      ggplot(aes(x = t, y = freqtile_power_c)) +
+      geom_line(aes(group = tile, alpha = tile), size = .25) +
+      geom_point(aes(group = tile, alpha = tile, color = red_zone, size = red_zone)) +
+      geom_text(aes(label = label), size = 3, position = position_nudge(x = 2, y = .000075)) + 
+      # geom_segment(aes(xend = t, group = tile, alpha = tile), yend = 0) +
+      # coord_trans(y = 'log') +
+      scale_x_continuous(breaks = c(1, seq(5, max(slice_power_per_t$t), 5)), minor_breaks = 1:max(slice_power_per_t$t)) + 
+      scale_color_manual(breaks = c(F, T), values = c('black', 'red')) +
+      scale_alpha_continuous(range = c(.5, 1), breaks = 1:max(slice_power_per_t$tile)) + 
+      scale_size_manual(breaks = c(F, T), values = c(.25, 1)) + 
+      facet_wrap(~subject+run) + 
+      theme(axis.text.x = element_text(size = 6))
+    # +
+    #   geom_rect(aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax), 
+    #             data = data.frame(xmin=c(10,80),xmax=c(40, 120), ymin=c(0,0), ymax=c(.016,.016), freqtile_power=c(0,0), t=c(0,0)))
+  }
+  if(parallelize){
+    didTry <- try(stopCluster(cl))
+  }
+}
+
+
+
