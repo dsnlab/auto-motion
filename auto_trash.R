f1c28d2dab0e67dbf260ed410c47d75778e0698d
Dani Cosme <dani.cosme@gmail.com>
2018-01-14 22:00:47 -0800

    modified with new rules for trash coding; incorporated stripe artifact info; changed plot params

--- auto_trash.R
+++ auto_trash.R
@@ -44,6 +44,33 @@ source('auto_trash_config.R')
 intensities = read.csv(paste0(outputDir,study,'_globalIntensities.csv'))
 
 #------------------------------------------------------
+# load stripe data
+#------------------------------------------------------
+# stripe file created using stripe_detect.R
+file_list = list.files(outputDir, pattern = stripePattern)
+
+for (file in file_list){
+  # if the merged dataset doesn't exist, create it
+  if (!exists("stripes")){
+    temp = read.csv(paste0(outputDir,file))
+    stripes = data.frame(temp) %>% 
+      rename("volume" = t) %>%
+      select(-file)
+    rm(temp)
+  }
+  
+  # if the merged dataset does exist, append to it
+  else {
+    temp_dataset = read.csv(paste0(outputDir,file))
+    temp_dataset = data.frame(temp_dataset) %>% 
+      rename("volume" = t) %>%
+      select(-file)
+    stripes = rbind(stripes, temp_dataset)
+    rm(temp_dataset)
+  }
+}
+
+#------------------------------------------------------
 # load rp_txt files and concatenate them
 #------------------------------------------------------
 # generate file list
@@ -51,12 +78,13 @@ file_list = list.files(rpDir, pattern = rpPattern)
 
 for (file in file_list){
   # if the merged dataset doesn't exist, create it
-  if (!exists("dataset")){
+  if (!exists("rp")){
     temp = read.table(paste0(rpDir,file))
     colnames(temp) = rpCols
-    dataset = data.frame(temp, file = rep(file,count(temp))) %>% 
+    rp = data.frame(temp, file = rep(file,count(temp))) %>% 
       mutate(volume = row_number()) %>%
       extract(file,c("subjectID","run"), rpPattern)
+    rm(temp)
   }
   
   # if the merged dataset does exist, append to it
@@ -66,21 +94,30 @@ for (file in file_list){
     temp_dataset = data.frame(temp_dataset, file = rep(file,count(temp_dataset))) %>% 
       mutate(volume = row_number()) %>%
       extract(file,c("subjectID","run"), rpPattern)
-    dataset = rbind(dataset, temp_dataset)
+    rp = rbind(rp, temp_dataset)
     rm(temp_dataset)
   }
 }
 
 #------------------------------------------------------
-# merge and create trash regressors
+# merge dataframes
 #------------------------------------------------------
-trash = intensities %>% 
-  mutate(subjectID = as.character(subjectID),
-         run = as.character(run)) %>%
-  left_join(., dataset, by = c("subjectID", "run", "volume")) %>%
-  group_by(subjectID, run) %>%
+joined = left_join(stripes, intensities, by = c("subjectID", "run", "volume")) %>%
+  left_join(., rp, by = c("subjectID", "run", "volume")) %>%
+  mutate(tile = paste0("tile_",tile)) %>%
+  group_by(subjectID, run, tile) %>%
   mutate(Diff.mean = volMean - lag(volMean),
          Diff.sd = volSD - lag(volSD)) %>%
+  spread(tile, freqtile_power)
+
+#------------------------------------------------------
+# create trash regressors 
+#------------------------------------------------------
+trash = joined %>%
+  gather(tile, freqtile_power, starts_with("tile")) %>%
+  filter(tile %in% c("tile_1", "tile_10")) %>%
+  
+  # code trash based on mean, sd, and rp
   ungroup %>%
   mutate(meanDiff.mean = mean(Diff.mean, na.rm=TRUE),
          sdDiff.mean = sd(Diff.mean, na.rm=TRUE),
@@ -88,26 +125,40 @@ trash = intensities %>%
          sdDiff.sd = sd(Diff.sd, na.rm=TRUE),
          
          # code volumes above mean thresholds as trash
-         trash.auto = ifelse(Diff.mean > (meanDiff.mean + 3*sdDiff.mean) | Diff.mean < (meanDiff.mean - 1.5*sdDiff.mean), 1, 0),
-         trash.auto = ifelse(Diff.sd > (meanDiff.sd + 3*sdDiff.sd) | Diff.sd < (meanDiff.sd - 3*sdDiff.sd), 1, trash.auto),
+         upper.mean = meanDiff.mean + 2*sdDiff.mean,
+         lower.mean = meanDiff.mean - 2*sdDiff.mean,
+         trash.mean = ifelse(Diff.mean > upper.mean | Diff.mean < lower.mean, 1, 0),
+         trash.mean = ifelse(is.na(Diff.mean),0,trash.mean),
          
-         # code volumes with more than +/- .3mm translation in Euclidian distance
-         trash.auto = ifelse(euclidian_trans_deriv > .3 | euclidian_trans_deriv < -.3, 1, trash.auto),
-         # code volumes with more than +/- .3mm translation in Euclidian distance
-         trash.auto = ifelse(euclidian_rot_deriv > .3 | euclidian_rot_deriv < -.3, 1, trash.auto),
+         upper.sd = meanDiff.sd + 2*sdDiff.sd,
+         lower.sd = meanDiff.sd - 2*sdDiff.sd,
+         trash.sd = ifelse(Diff.sd > upper.sd | Diff.sd < lower.sd, 1, 0),
+         trash.sd = ifelse(is.na(Diff.sd),0,trash.sd),
          
-         # recode as trash if volume behind and in front are both marked as trash
-         trash.auto = ifelse(trash.auto == 0 & lag(trash.auto) == 1 & lead(trash.auto) == 1, 1, trash.auto),
-         
-         # reduce false positives on last volume in motion sequence
-         trash.auto = ifelse((trash.auto == 1 & lag(trash.auto == 1) & lead(trash.auto == 0)) & lag(!is.na(Diff.mean)) & (Diff.mean < (meanDiff.mean + 1.5*sdDiff.mean) & Diff.mean > (meanDiff.mean - 3*sdDiff.mean)), 0, trash.auto),
-         # reduce false negatives before trash volume
-         trash.auto = ifelse((trash.auto == 0 & lead(trash.auto == 1)) & (Diff.mean > (meanDiff.mean + sdDiff.mean) | Diff.mean < (meanDiff.mean - sdDiff.mean)), 1, trash.auto),
-         
-         # code first volume as trash if second volume is trash
-         trash.auto = ifelse(volume == 1 & lead(trash.auto) == 1, 1, trash.auto)) %>%
-  mutate(trash.combined = ifelse(trash.rp == 1 | trash.auto == 1, 1, 0)) %>%
-  select(subjectID, run, volume, Diff.mean, Diff.sd, volMean, volSD, starts_with("euclidian"), trash.rp, trash.auto, trash.combined)
+         # code volumes with more than +/- .25mm translation or rotation in Euclidian distance
+         trash.rp.tr = ifelse(euclidian_trans_deriv > .25 | euclidian_trans_deriv < -.25, 1, 0),
+         trash.rp.rot = ifelse(euclidian_rot_deriv > .25 | euclidian_rot_deriv < -.25, 1, 0)) %>%
+  select(-meanDiff.mean, -meanDiff.sd, -sdDiff.mean, -sdDiff.sd) %>%
+  
+  # code trash based on striping
+  group_by(subjectID, run, tile) %>%
+  mutate(freqtile_power_c = freqtile_power - mean(freqtile_power, na.rm=TRUE)) %>%
+  ungroup() %>%
+  select(-freqtile_power) %>%
+  spread(tile,freqtile_power_c) %>%
+  mutate(trash.stripe = ifelse(tile_1 < -.035 & tile_10 > .00025, 1, 0)) %>%
+  
+  # combine trash
+  mutate(trash.combined = ifelse(trash.stripe == 1, 1, 0),
+         trash.sum = trash.rp.tr + trash.rp.rot + trash.mean + trash.sd + trash.stripe,
+         trash.combined = ifelse((trash.rp.tr + trash.rp.rot + trash.mean + trash.sd) > 1, 1, trash.combined)) %>%
+  
+  # recode as trash if volume behind and in front are both marked as trash
+  mutate(trash.combined = ifelse(trash.combined == 0 & lag(trash.combined) == 1 & lead(trash.combined) == 1, 1, trash.combined)) %>%
+  
+  # code first volume as trash if second volume is trash
+  mutate(trash.combined = ifelse(volume == 1 & lead(trash.combined) == 1, 1, trash.combined)) %>%
+  select(subjectID, run, volume, Diff.mean, Diff.sd, volMean, volSD, starts_with("euclidian"), tile_1, tile_10, trash.rp.tr, trash.rp.rot, trash.mean, trash.sd, trash.stripe, trash.combined)
 
 #------------------------------------------------------
 # write auto trash csv
@@ -139,50 +190,62 @@ write.csv(summary.trash, paste0(outputDir,study,'_trashVols.csv'), row.names = F
 # write rp_txt files
 #------------------------------------------------------
 if (writeRP){
-rp = trash %>%
-  select(subjectID, run, volume, starts_with("euclidian"), trash.combined) %>%
-  mutate(trash.combined = ifelse(is.na(trash.combined), 0, trash.combined))
+  if(!file.exists(rpOutputDir)){
+    message(paste0(rpOutputDir, ' does not exist. Creating it...'))
+    dir.create(rpOutputDir)
+  }
   
-rp_files_written = rp %>% 
-  arrange(subjectID, run, volume) %>% 
-  group_by(subjectID, run) %>% 
-  do({
-    fname=paste(
-      rpOutputDir,
-      'rp_',.$subjectID[[1]],'_',.$run[[1]],'.txt',
-      sep='')
-    write.table(
-      .[,c(-1,-2,-3)],
-      fname,
-      quote=F,
-      sep='   ',
-      row.names=F,
-      col.names=F)
-    data.frame(rp_file_name=fname)
-  })
+  rp = trash %>%
+    select(subjectID, run, volume, starts_with("euclidian"), trash.combined) %>%
+    mutate(trash.combined = ifelse(is.na(trash.combined), 0, trash.combined))
+  
+  rp_files_written = rp %>% 
+    arrange(subjectID, run, volume) %>% 
+    group_by(subjectID, run) %>% 
+    do({
+      fname=paste(
+        rpOutputDir,
+        'rp_',.$subjectID[[1]],'_',.$run[[1]],'.txt',
+        sep='')
+      write.table(
+        .[,c(-1,-2,-3)],
+        fname,
+        quote=F,
+        sep='   ',
+        row.names=F,
+        col.names=F)
+      data.frame(rp_file_name=fname)
+    })
 }
 
 #------------------------------------------------------
 # plot data for each subject
 #------------------------------------------------------
 if (writePlots){
+  if(!file.exists(plotDir)){
+    message(paste0(plotDir, ' does not exist. Creating it...'))
+    dir.create(plotDir)
+  }
+  
   # visualize for each subject subject
   trash.plot = trash %>%
-    mutate(trash.combined = ifelse(is.na(trash.combined), 0, trash.combined),
-          code = ifelse(trash.combined == 1, "trash", "not trash")) %>%
-    select(-starts_with("Diff"), -starts_with("trash")) %>%
-    gather(measure, value, -c(subjectID, run, volume, code))
+    mutate(trash.combined = ifelse(trash.combined == 0, NA, trash.combined),
+          code = ifelse(trash.combined == 1, "trash", ""),
+          label = ifelse(regexpr('.*', trash.combined), as.character(volume), '')) %>%
+    select(-volMean, -volSD, -euclidian_trans, -euclidian_rot, -starts_with("trash")) %>%
+    gather(measure, value, -c(subjectID, run, volume, code, label))
   
   nada = trash.plot %>% group_by(subjectID) %>%
     do({
       plot = ggplot(., aes(volume, value)) + 
-        geom_point(aes(color = code)) + 
-        geom_line() + 
+        geom_line(size = .25) +
+        geom_point(data = subset(., !is.na(code)), aes(color = code), size = 3) +
+        geom_text(aes(label = label), size = 1.5) +
         facet_grid(measure ~ run, scales= "free") +
-        scale_colour_discrete(drop = FALSE) + 
+        scale_colour_manual(values = "#E4B80E", drop = FALSE)
         labs(title = .$subjectID[[1]])
       print(plot)
-      ggsave(plot, file=paste0(plotDir,.$subjectID[[1]],'.pdf'), height = 10, width = 12)
+      ggsave(plot, file=paste0(plotDir,.$subjectID[[1]],'.pdf'), height = 12, width = 14)
       data.frame()
     })
 }
